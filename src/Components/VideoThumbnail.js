23825d5794d8b366d94c81345bb7de247117dfd2
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-11-15 12:59:33 -0500

    css fix

--- src/Components/VideoThumbnail.js
+++ src/Components/VideoThumbnail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import "./VideoThumbnail.css"
 function VideoThumbnail({video, videoId}) {
 const [views, setViews] = useState("")
 /* this fetch w/ 'statistics gives viewcount
