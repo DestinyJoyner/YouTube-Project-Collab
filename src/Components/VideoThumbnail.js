da24abf757d36c558662566b454b0c49e8d3f652
DestinyJoyner <destinyjoyner@pursuit.org>
2022-11-15 14:02:19 -0500

    removed commment

--- src/Components/VideoThumbnail.js
+++ src/Components/VideoThumbnail.js
@@ -5,7 +5,7 @@ import "./VideoThumbnail.css"
 function VideoThumbnail({video, videoId}) {
 const [views, setViews] = useState("")
 /* this fetch w/ 'statistics gives viewcount
-    https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2C%20statistics&id=ZtHCnXMjIXY&maxResults=25&key=AIzaSyBPnE3WELPDQRQlYWCpMDN9Gw_B5GjcoEE
+    https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2C%20statistics&id=ZtHCnXMjIXY&maxResults=25&key=
 */
     useEffect(() => {
         fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2C%20statistics&id=${videoId}&maxResults=25&key=${process.env.REACT_APP_API_KEY}`)
