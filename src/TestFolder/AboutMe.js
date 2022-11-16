1a311b276e8b0ffa94792acd6751f9a9486a6974
DestinyJoyner <destinyjoyner@pursuit.org>
2022-11-16 18:58:50 -0500

    minor changes

--- src/TestFolder/AboutMe.js
+++ src/TestFolder/AboutMe.js
@@ -8,29 +8,29 @@ function AboutMe(props) {
 
     
     return (
-        <div className = 'about'>
+        // <div className = 'about'>
             
-            <div className='lightining'></div>
-            <div className='rain'></div>
-            <div className='axe-hammer'><img 
-            src = {pix}
-            alt = 'collision' /></div>
-        </div>
-        // <div className='aboutMe'>
-        // <h1><span className= "destiny">Destiny</span>  <span id ="dan">Dan</span></h1>
-        // <div class="gallery">
-        //     <article className='destiny'>
-        //         <img src={profile} alt="destiny" className='destinypic'
-        //         />
-        //         <p>
-        //             <li>Destiny</li>
-        //             <li>App Crusher</li>
-        //             <li>Web developer</li>
-        //         </p>
-        //             </article>
-        //     <img src="https://assets.codepen.io/1480814/saber.jpg" alt="Saber from Fate/Stay"/>
+        //     <div className='lightining'></div>
+        //     <div className='rain'></div>
+        //     <div className='axe-hammer'><img 
+        //     src = {pix}
+        //     alt = 'collision' /></div>
         // </div>
-        // </div>   
+        <div className='aboutMe'>
+        <h1><span className= "destiny">Destiny</span>  <span id ="dan">Dan</span></h1>
+        <div class="gallery">
+            <article className='destiny'>
+                <img src={profile} alt="destiny" className='destinypic'
+                />
+                <p>
+                    <li>Destiny</li>
+                    <li>App Crusher</li>
+                    <li>Web developer</li>
+                </p>
+                    </article>
+            <img src="https://assets.codepen.io/1480814/saber.jpg" alt="Saber from Fate/Stay"/>
+        </div>
+        </div>   
     );
 }
 
