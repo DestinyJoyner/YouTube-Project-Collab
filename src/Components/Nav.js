5398d98c9d258f2c8068f73507ae0cb7d9b5df4a
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-11-17 18:07:36 -0500

    added logo

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import youTubeLogo from "./assets/youTubeLogo.png"
 import "./Nav.css"
 
 function Nav(props) {
     return (
         <nav className='nav'>
+            <img src={youTubeLogo} alt="youTube Logo" height="30px"/>
             <h1>YouTube</h1>
             <Link to = "/" ><h2>Home</h2></Link>
             <Link to = "/about"><h2>About</h2></Link>
