8d5ff47ce8ba536150272df80d8e2381959d728d
DestinyJoyner <destinyjoyner@pursuit.org>
2022-11-20 06:52:25 -0500

    easter egg additional css

--- src/Easter Egg/EasterEgg.js
+++ src/Easter Egg/EasterEgg.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import rain from './rain.png'
 import pix from './kratos-thor.gif'
-import destiny from './gaming-in-chair.png'
-// import "./EasterEgg.css";
+import destiny from './short.png'
+import dan from './test.png'
+
+
+import "./EasterEgg.css"
 
 function EasterEgg(props) {
+    
+    
     return (
         <div className = 'about'>
             
-            <div className='lightining'></div>
-            <div className='rain'></div>
+            <div className='lightining'
+            ></div>
+            <div className='rain'
+            style={{backgroundImage: `url(${rain})`}}></div>
             <div className='axe-hammer'><img 
             src = {pix}
             alt = 'collision' /></div>
@@ -17,15 +25,21 @@ function EasterEgg(props) {
             <div className="gallery">
            
             <article className='destiny'>
-                <img src={destiny} alt="destiny" className='destinypic'
-                />
+                <img src={destiny} alt="destiny" />
                 <p>
                     <li>Destiny</li>
                     <li>App Crusher</li>
                     <li>Web developer</li>
                 </p>
                     </article>
-            <img src="https://assets.codepen.io/1480814/saber.jpg" alt="Saber from Fate/Stay"/>
+            <article className='dan'>
+                <img src={dan} alt="danpic"/>
+                <p>
+                    <li>Dan</li>
+                    <li>App Crusher</li>
+                    <li>Web developer</li>
+                </p>
+            </article>
         </div>
         </div>
     );
